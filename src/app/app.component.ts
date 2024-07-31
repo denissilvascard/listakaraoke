91f4b33acd976935b24dbfdef905f16a534826b5
DenisSilvaScard <denissilva@pactosolucoes.com.br>
2024-07-31 19:23:04 -0300

    Criando primeira lista teste

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Lista de Musícas', url: '/folder/Lista', icon: 'mail' },
+    { title: 'Listas de Musícas', url: '/folder/Lista', icon: 'musical-notes' },
     { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
     { title: 'Archived', url: '/folder/archived', icon: 'archive' },
