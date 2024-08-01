32cd9a6c1a3a22c94c22c9196f5dd8e38671ea7e
DenisSilvaScard <denissilva@pactosolucoes.com.br>
2024-08-01 20:53:31 -0300

    removendo scroll de vez

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {HEROES} from "../mock-heroes";
 import {Hero} from "../hero";
-import {InfiniteScrollCustomEvent} from "@ionic/angular";
+
 import {FormBuilder} from "@angular/forms";
 
 @Component({
@@ -28,29 +28,15 @@ export class FolderPage implements OnInit {
   }
 
   private generateItems() {
-    const count = this.heroes.length + 1;
-    // for (let i = 0; i < 50; i++) {
-    //   // this.heroes[i].musica = (this.heroes[i].musica.length > 10 ? this.heroes[i].musica.substr(1, 10) : this.heroes[i].musica);
-    //   this.items.push(this.heroes[i]);
-    // }
+
     this.items = this.heroes;
     this.items.sort((a,b) => a.musica.localeCompare(b.musica));
-    // this.consultarMusica();
 
   }
 
-  onIonInfinite(ev) {
-    this.generateItems();
-    setTimeout(() => {
-      (ev as InfiniteScrollCustomEvent).target.complete();
-    }, 500);
-  }
 
 
-  // consultarMusica(){
-  //       var index = this.items.findIndex(obj => obj.musica === "Azul");
-  //       this.itemsConsulta.push(this.heroes[index]);
-  // }
+
   filtrarPessoas(): Hero[] {
 
     if (!this.buscaNome) {
