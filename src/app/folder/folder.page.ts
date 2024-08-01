a373772ae69825626bf626bfc249c5947e054968
DenisSilvaScard <denissilva@pactosolucoes.com.br>
2024-08-01 14:50:14 -0300

    Populando infinite-scroll

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -2,6 +2,7 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {HEROES} from "../mock-heroes";
 import {Hero} from "../hero";
+import {InfiniteScrollCustomEvent} from "@ionic/angular";
 
 @Component({
   selector: 'app-folder',
@@ -12,11 +13,28 @@ export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
   constructor() {}
-
+  items = [];
   heroes = HEROES;
   selectedHero?: Hero;
 
   ngOnInit() {
+    this.generateItems();
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
+
+  private generateItems() {
+    const count = this.heroes.length + 1;
+    for (let i = 0; i < 50; i++) {
+      this.items.push(this.heroes[i]);
+    }
+  }
+
+  onIonInfinite(ev) {
+    this.generateItems();
+    setTimeout(() => {
+      (ev as InfiniteScrollCustomEvent).target.complete();
+    }, 500);
+  }
+
+
 }
